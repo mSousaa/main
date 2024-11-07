3e04a654e5ccce0abf6c45e53bc5b82f4f32638c
mSousaa <matheussousa851@gmail.com>
2024-11-07 20:18:51 -0300

    test-middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,19 @@ const router = require("./router");
 const app = express();
 
 app.use(express.json()); 
+
+app.use((req, res, next) =>{
+    const token = req.headers.token || null;
+
+    try{
+      if(token !== "123")
+        throw "Token inválido";
+    }catch(e){
+      return res.status(403).send("barrado" + e)
+    }
+    next()
+})
+
 app.use(router); 
 
 app.get("/", (req, res) => {
